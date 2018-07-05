8b32218c597057f2c02531d85516e4c2360dd461
helsey33 <helseysin827@gmail.com>
2018-07-05 15:23:26 +0530

    create generate message utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http =require('http');
 const socketIO = require('socket.io');
 const express = require('express');
 
+var {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -14,30 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New User connected');
 
-    socket.emit('newMessage',{
-        from:'Admin',
-        text: 'Welcome to the chat room',
-        createdAt: new Date().getTime()
-    });
-    socket.broadcast.emit('newMessage',{
-        from:'Admin',
-        text:'newUserJoined',
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app!'));
+    socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined'));
     
 
     socket.on('createMessage',(newMessage)=>{
         console.log('createMessage',newMessage);
-        io.emit('newMessage',{
-            from:newMessage.from,
-            text:newMessage.text,
-            createdAt:new Date().getTime()
-        })
-    })
+        io.emit('newMessage',generateMessage(message.from,message.text));
+    });
 
     socket.on('disconnect',()=>{
         console.log('User disconnected');
-    })
+    });
 })
 
 
