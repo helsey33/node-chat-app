7b6d01bb7175f39788eb0db480eae8a50017914a
helsey33 <helseysin827@gmail.com>
2018-07-05 15:14:23 +0530

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
     console.log('New User connected');
 
+    socket.emit('newMessage',{
+        from:'Admin',
+        text: 'Welcome to the chat room',
+        createdAt: new Date().getTime()
+    });
+    socket.broadcast.emit('newMessage',{
+        from:'Admin',
+        text:'newUserJoined',
+        createdAt: new Date().getTime()
+    });
     
 
     socket.on('createMessage',(newMessage)=>{
